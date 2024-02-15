7468fb1d55e4cae46d458263e9a3539308f026ad
Ahmed Belhaj Salah <ahmedbhs39@gmail.com>
2024-02-15 21:57:44 +0100

    add get All cars functionality for customer view

--- frontend/car_rental_frontend/src/app/modules/customer/components/customer-dashboard/customer-dashboard.component.ts
+++ frontend/car_rental_frontend/src/app/modules/customer/components/customer-dashboard/customer-dashboard.component.ts
@@ -1,10 +1,81 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CustomerService } from '../../services/customer.service';
+import { Cars } from '../../../admin/components/admin-dashboard/admin-dashboard.component';
 
 @Component({
   selector: 'app-customer-dashboard',
   templateUrl: './customer-dashboard.component.html',
   styleUrl: './customer-dashboard.component.css'
 })
-export class CustomerDashboardComponent {
+export class CustomerDashboardComponent implements OnInit {
+
+  cars: Cars[] = [{
+    id: 1,
+    name: "BMW",
+  brand: "BMW",
+  color: "Black",
+  type: "X7",
+  transmission: "test",
+  description: " in a good situation",
+  price: 33,
+  date: new Date("07/01/2022"),
+  image: "https://material.angular.io/assets/img/examples/shiba2.jpg"
+  },
+  {
+    id: 2,
+    name: "Audi",
+  brand: "Audi",
+  color: "Black",
+  type: "S3",
+  transmission: "test",
+  description: " in a good situation",
+  price: 33,
+  date: new Date("07/01/2022"),
+  image: "https://material.angular.io/assets/img/examples/shiba2.jpg"
+  },
+  {
+    id: 3,
+    name: "Mercedes",
+  brand: "Mercedes",
+  color: "Black",
+  type: "M4",
+  transmission: "test",
+  description: " in a good situation",
+  price: 33,
+  date: new Date("07/01/2022"),
+  image: "https://material.angular.io/assets/img/examples/shiba2.jpg"
+  },
+  {
+    id: 4,
+    name: "Volkswagen",
+  brand: "Volkswagen",
+  color: "Black",
+  type: "V6",
+  transmission: "test",
+  description: " in a good situation",
+  price: 33,
+  date: new Date("07/01/2022"),
+  image: "https://material.angular.io/assets/img/examples/shiba2.jpg"
+  },
+  
+];
+
+
+  constructor(private customerService: CustomerService){}
+
+
+  ngOnInit(): void {
+    //this.getAllCars(); //to uncomment it once we don't have forbidden access
+  }
+
+  getAllCars(){
+    this.customerService.getAllCars().subscribe(data => {
+      data.forEach((element: any) => {
+        element.processedImg = 'data:image/jpeg;base64,' + element.returnedImage;
+        this.cars.push(element);
+      });
+    })
+  }
+
 
 }
--- frontend/car_rental_frontend/src/app/modules/customer/services/customer.service.ts
+++ frontend/car_rental_frontend/src/app/modules/customer/services/customer.service.ts
@@ -1,9 +1,32 @@
-import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Inject, Injectable, PLATFORM_ID } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Cars } from '../../admin/components/admin-dashboard/admin-dashboard.component';
+import { isPlatformBrowser } from '@angular/common';
+import { StorageService } from '../../../auth/services/storage/storage.service';
+
+
+const BASE_URL = "http://localhost:8080";
 
 @Injectable({
   providedIn: 'root'
 })
 export class CustomerService {
 
-  constructor() { }
+  constructor(private http: HttpClient, @Inject(PLATFORM_ID) private platformId: Object) { }
+
+  getAllCars(): Observable<Cars[]> {
+    return this.http.get<Cars[]>(BASE_URL + "/api/admin/cars", { headers: this.createAuthorizationHeader() });
+  }
+
+  createAuthorizationHeader(): HttpHeaders {
+    let authHeaders: HttpHeaders = new HttpHeaders();
+    if (isPlatformBrowser(this.platformId)) {
+      const token = StorageService.getToken();
+      if (token) {
+        return authHeaders.set('Authorization', 'Bearer ' + token);
+      }
+    }
+    return authHeaders;
+  }
 }
